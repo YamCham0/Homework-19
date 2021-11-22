8b9f5f8abc3d5bb0a2f5f6c588447844aa7b75f6
Yamil Chamo <yajochakha@gmail.com>
2021-11-22 01:10:28 -0600

    Readme

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
   '/db.js',
   '/index.js',
   '/styles.css',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
   '/manifest.webmanifest'
 
 ];
